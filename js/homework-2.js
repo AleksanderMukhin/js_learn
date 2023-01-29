4db78d1adc39f71d64ff91f34d6d42711b4441c6
AleksanderMukhin <mukhin.aleksander.ua@gmail.com>
2023-01-29 11:06:08 +0100

    add_inexOf

--- js/homework-2.js
+++ js/homework-2.js
@@ -3,3 +3,8 @@ const words = ["JavaScript", "это", "интересно"];
 console.log(words.join("")); // "JavaScriptэтоинтересно"
 console.log(words.join(" ")); // "JavaScript это интересно"
 console.log(words.join("-")); 
+
+
+const clients = ["Mango", "Ajax", "Poly", "Kiwi"];
+console.log(clients.indexOf("Poly")); // 2
+console.log(clients.indexOf("Mango"))
