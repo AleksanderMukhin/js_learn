8194d3ea2a93ce1ea97cf11d506d07d0ac969eaf
AleksanderMukhin <mukhin.aleksander.ua@gmail.com>
2023-02-07 11:58:41 +0100

    task 19-20

--- js/homework-3.js
+++ js/homework-3.js
@@ -364,3 +364,71 @@
 
 
 //----task 18----
+// const products = [
+//   { name: "Radar", price: 1300, quantity: 4 },
+//   { name: "Scanner", price: 2700, quantity: 3 },
+//   { name: "Droid", price: 400, quantity: 7 },
+//   { name: "Grip", price: 1200, quantity: 9 },
+// ];
+
+// function getProductPrice(productName) {
+//   // Change code below this line
+//   for (const product of products) {
+//     if (product.name === productName) {
+//       return product.price;
+//   }
+// }
+
+// return null;
+//   // Change code above this line
+// }
+// console.log(getProductPrice("Radar"));
+
+
+//-----task 19------
+// const products = [
+//   { name: "Radar", price: 1300, quantity: 4 },
+//   { name: "Scanner", price: 2700, quantity: 3 },
+//   { name: "Droid", price: 400, quantity: 7 },
+//   { name: "Grip", price: 1200, quantity: 9 },
+// ];
+
+// function getAllPropValues(propName) {
+//   // Change code below this line
+//   const values = [];
+  
+//   for (let product of products) {
+//     const key = Object.keys(product);
+//     if (key.includes(propName)) {
+//     values.push(product[propName])
+//     }    
+// }  
+// return values;
+//   // Change code above this line
+// }
+// console.log(getAllPropValues("name"));
+
+
+//----way 2 task 19----
+// const products = [
+//   { name: "Radar", price: 1300, quantity: 4 },
+//   { name: "Scanner", price: 2700, quantity: 3 },
+//   { name: "Droid", price: 400, quantity: 7 },
+//   { name: "Grip", price: 1200, quantity: 9 },
+// ];
+
+// function getAllPropValues(propName) {
+//   // Change code below this line
+//   let values = [];
+//     for (const product of products) {
+//         if (propName in product) {
+//             values.push(product[propName]);
+//         }
+//     }
+//     return values;
+//   // Change code above this line
+// }
+// console.log(getAllPropValues("name"));
+
+
+//-----task 20----
