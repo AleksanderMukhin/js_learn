4df4fa794cd084f6d71251f8a07837ce737df5f1
AleksanderMukhin <mukhin.aleksander.ua@gmail.com>
2023-02-07 18:39:58 +0100

    task 21-25

--- js/homework-3.js
+++ js/homework-3.js
@@ -398,6 +398,7 @@
 //   const values = [];
   
 //   for (let product of products) {
+//     console.log(product)
 //     const key = Object.keys(product);
 //     if (key.includes(propName)) {
 //     values.push(product[propName])
@@ -432,3 +433,56 @@
 
 
 //-----task 20----
+// const products = [
+//   { name: "Radar", price: 1300, quantity: 4 },
+//   { name: "Scanner", price: 2700, quantity: 3 },
+//   { name: "Droid", price: 400, quantity: 7 },
+//   { name: "Grip", price: 1200, quantity: 9 },
+// ];
+
+// function calculateTotalPrice(productName) {
+//   // Пиши код ниже этой строки
+// let total = 0;
+//   for (const product of products) {
+//     if (product.name === productName) {
+//       total = product.price * product.quantity;       
+//     }     
+//   }  
+//   return total;
+
+//   // Пиши код выше этой строки
+// }
+// console.log(calculateTotalPrice("Radar"));
+// console.log(calculateTotalPrice("Blaster"));
+// console.log(calculateTotalPrice("Grip"))
+
+
+//-----task 24-----
+// const colors = [
+//   { hex: "#f44336", rgb: "244,67,54" },
+//   { hex: "#2196f3", rgb: "33,150,243" },
+//   { hex: "#4caf50", rgb: "76,175,80" },
+//   { hex: "#ffeb3b", rgb: "255,235,59" },
+// ];
+
+// const hexColors = [];
+// const rgbColors = [];
+// // Change code below this line
+
+// for (const {hex, rgb} of colors) {
+//   hexColors.push(hex);
+//   rgbColors.push(rgb);
+// }
+// console.log(hexColors)
+
+
+//----task 25----
+function calculateMeanTemperature(forecast) {
+  
+    const {today: { low: todayLow, high: todayHigh}, tomorrow: {low: tomorrowLow, high: tomorrowHigh}} = forecast;
+  
+
+  // Change code above this line
+  return (todayLow + todayHigh + tomorrowLow + tomorrowHigh) / 4;
+}
+console.log(calculateMeanTemperature({ today: {low: 28, high: 32}, tomorrow: {low: 25, high: 29} }))
