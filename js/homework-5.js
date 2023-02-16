66c113f92af8936988a36a5902148be433e1ba91
AleksanderMukhin <mukhin.aleksander.ua@gmail.com>
2023-02-16 16:24:14 +0100

    class

--- js/homework-5.js
+++ js/homework-5.js
@@ -13,12 +13,128 @@
 // bookShelf.addAuthor("Ли Танит");
 // console.log(bookShelf.getAuthors()); 
 
-const baseSalary = 30000;
-const overtime = 10;
-const rate = 20;
+// const baseSalary = 30000;
+// const overtime = 10;
+// const rate = 20;
 
-const getWage = (baseSalary, overtime, rate) => {
-  return baseSalary + overtime * rate;
+// const getWage = (baseSalary, overtime, rate) => {
+//   return baseSalary + overtime * rate;
+// };
+
+// console.log(getWage(baseSalary, overtime, rate));
+
+
+// const animal = { eats: true };
+// const dog = Object.create(animal);
+// dog.barks = true;
+
+// for (const key in dog) {
+//   console.log(key); // barks, eats
+// }
+
+
+// const ancestor = {
+//   name: "Paul",
+//   age: 83,
+//   surname: "Dawson",
+//   heritage: "Irish",
+// };
+// // Change code below this line
+
+// const parent = Object.create(ancestor);
+// parent.name = "Stacey";
+// parent.surname = "Moore";
+// parent.age = 54;
+
+// const child = Object.create(parent);
+// child.name = "Jason";
+// child.age = 27;
+
+// console.log(child.name);
+
+
+// class User {
+//   constructor(name, email) {
+//     // Инициализация свойств экземпляра
+//     this.name = name;
+//     this.email = email;
+//   }
+// }
+
+// const mango = new User("Манго", "mango@mail.com");
+// console.log(mango); // {}
+
+// const poly = new User();
+// console.log(poly);
+
+
+// class User {
+//   constructor({ name, email }) {
+//     this.name = name;
+//     this.email = email;
+//   }
+
+//   // Метод getEmail
+//   getEmail() {
+//     return this.email;
+//   }
+
+//   // Метод changeEmail
+//   changeEmail(newEmail) {
+//     this.email = newEmail;
+//   }
+// };
+
+
+
+// class User {
+//   // Необязательное объявление публичных свойств
+//   name;
+//   // Обязательное объявление приватных свойств
+//   #email;
+
+//   constructor({ name, email }) {
+//     this.name = name;
+//     this.#email = email;
+//   }
+
+//   getEmail() {
+//     return this.#email;
+//   }
+
+//   changeEmail(newEmail) {
+//     this.#email = newEmail;
+//   }
+// }
+
+// const mango = new User({
+//   name: "Манго",
+//   email: "mango@mail.com",
+// });
+// mango.changeEmail("mango@supermail.com");
+// console.log(mango.getEmail()); // mango@supermail.com
+// console.log(mango.#email); // Будет ошибка, это приватное свойство
+
+
+class User {
+  #email;
+
+  constructor({ name, email }) {
+    this.name = name;
+    this.#email = email;
+  }
+
+  // Геттер email
+  get email() {
+    return this.#email;
+  }
+
+  // Сеттер email
+  set email(newEmail) {
+    this.#email = newEmail;
+  }
 };
-
-console.log(getWage(baseSalary, overtime, rate));
+const mango = new User({ name: "Манго", email: "mango@mail.com" });
+console.log(mango.email); // mango@mail.com
+mango.email = "mango@supermail.com";
+console.log(mango.email); // mango@supermail.com
