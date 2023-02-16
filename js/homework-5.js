871ec5dc61974177fb2f858ca3720ee6254a2b67
AleksanderMukhin <mukhin.aleksander.ua@gmail.com>
2023-02-16 15:01:40 +0100

    method fn

--- js/homework-5.js
+++ js/homework-5.js
@@ -1,13 +1,24 @@
-const bookShelf = {
-  authors: ["Бернард Корнуэлл", "Роберт Шекли"],
-  getAuthors() {
-    return this.authors;
-  },
-  addAuthor(authorName) {
-    this.authors.push(authorName);
-  },
+//-------------theory-----------------
+// const bookShelf = {
+//   authors: ["Бернард Корнуэлл", "Роберт Шекли"],
+//   getAuthors() {
+//     return this.authors;
+//   },
+//   addAuthor(authorName) {
+//     this.authors.push(authorName);
+//   },
+// };
+
+// console.log(bookShelf.getAuthors()); // ["Бернард Корнуэлл", "Роберт Шекли"]
+// bookShelf.addAuthor("Ли Танит");
+// console.log(bookShelf.getAuthors()); 
+
+const baseSalary = 30000;
+const overtime = 10;
+const rate = 20;
+
+const getWage = (baseSalary, overtime, rate) => {
+  return baseSalary + overtime * rate;
 };
 
-console.log(bookShelf.getAuthors()); // ["Бернард Корнуэлл", "Роберт Шекли"]
-bookShelf.addAuthor("Ли Танит");
-console.log(bookShelf.getAuthors()); 
+console.log(getWage(baseSalary, overtime, rate));
