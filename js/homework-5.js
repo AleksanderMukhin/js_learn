d157781ee312a99a9ef3dd4d6abe09457ffbf153
AleksanderMukhin <mukhin.aleksander.ua@gmail.com>
2023-02-19 09:33:48 +0100

    prototype

--- js/homework-5.js
+++ js/homework-5.js
@@ -116,25 +116,48 @@
 // console.log(mango.#email); // Будет ошибка, это приватное свойство
 
 
-class User {
-  #email;
+// class User {
+//   #email;
 
-  constructor({ name, email }) {
-    this.name = name;
-    this.#email = email;
-  }
+//   constructor({ name, email }) {
+//     this.name = name;
+//     this.#email = email;
+//   }
 
-  // Геттер email
-  get email() {
-    return this.#email;
-  }
+//   // Геттер email
+//   get email() {
+//     return this.#email;
+//   }
 
-  // Сеттер email
-  set email(newEmail) {
-    this.#email = newEmail;
-  }
+//   // Сеттер email
+//   set email(newEmail) {
+//     this.#email = newEmail;
+//   }
+// };
+// const mango = new User({ name: "Манго", email: "mango@mail.com" });
+// console.log(mango.email); // mango@mail.com
+// mango.email = "mango@supermail.com";
+// console.log(mango.email); // mango@supermail.com
+
+
+
+const Car = function ({ brand, model, price } = {}) {
+   
+  this.brand = brand;
+  this.model = model;
+  this.price = price;
+
+  
 };
-const mango = new User({ name: "Манго", email: "mango@mail.com" });
-console.log(mango.email); // mango@mail.com
-mango.email = "mango@supermail.com";
-console.log(mango.email); // mango@supermail.com
+console.log(Car.prototype);
+
+
+const myCar = new Car({
+  brand: 'audi',
+  model: 'Q3',
+  price: '20000',
+});
+console.log(myCar);
+
+const yourCar = new Car(25);
+console.log(yourCar)
