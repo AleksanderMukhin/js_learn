e67456c982c8332be41b9ca34f65e22473c5dba0
AleksanderMukhin <mukhin.aleksander.ua@gmail.com>
2023-02-19 16:42:08 +0100

    task 16-20

--- js/homework-5.js
+++ js/homework-5.js
@@ -228,23 +228,139 @@
 
 
 //-------task 12-------------
-class Car {
+// class Car {
+//   // Change code below this line
+// #brand;
+//   constructor({ brand, model, price }) {
+//     this.#brand = brand;
+//     this.model = model;
+//     this.price = price;
+//   }
+
+//   getBrand(){
+//     return this.#brand;
+//   }
+
+//   changeBrand(newBrand) {
+//     this.#brand = newBrand;
+//   }
+//   // Change code above this line
+// }
+
+// console.log(new Car({ brand: "Audi", model: "Q3", price: 36000 }));
+
+
+//----------task 16-----------
+// class Car {
+//   // Change code below this line
+// static MAX_PRICE = 50000;
+  
+//   #price;
+
+//   constructor({ price }) {
+//     this.#price = price;
+//   }
+
+//   get price() {
+//     return this.#price;
+//   }
+
+//   set price(newPrice) {
+//     if(newPrice < Car.MAX_PRICE) {
+//       this.#price = newPrice; 
+//     }
+  
+//   }
+//   // Change code above this line
+// }
+
+// const audi = new Car({ price: 35000 });
+// console.log(audi.price); // 35000
+
+// audi.price = 49000;
+// console.log(audi.price); // 49000
+
+// audi.price = 51000;
+// console.log(audi.price); // 49000
+
+
+//-------task 17-------
+// class Car {
+//   static #MAX_PRICE = 50000;
+//   // Change code below this line
+// static checkPrice(price) {
+//   if(price > Car.#MAX_PRICE) {
+//     return "Error! Price exceeds the maximum";
+//   }
+//   return "Success! Price is within acceptable limits"
+// }
+//   // Change code above this line
+//   constructor({ price }) {
+//     this.price = price;
+//   }
+// }
+
+// const audi = new Car({ price: 36000 });
+// const bmw = new Car({ price: 64000 });
+
+// console.log(Car.checkPrice(audi.price)); // "Success! Price is within acceptable limits"
+// console.log(Car.checkPrice(bmw.price)); // "Error! Price exceeds the maximum"
+
+
+
+//----------task 20------
+class User {
+  email;
+
+  constructor(email) {
+    this.email = email;
+  }
+
+  get email() {
+    return this.email;
+  }
+
+  set email(newEmail) {
+    this.email = newEmail;
+  }
+}
+class Admin extends User {
   // Change code below this line
-#brand;
-  constructor({ brand, model, price }) {
-    this.#brand = brand;
-    this.model = model;
-    this.price = price;
+
+  static AccessLevel = {
+    BASIC: "basic",
+    SUPERUSER: "superuser",
+  };
+
+  constructor({ email, accessLevel }) {
+    super(email);
+    this.accessLevel = accessLevel;
   }
 
-  getBrand(){
-    return this.#brand;
+  blacklistedEmails = [];
+  blacklist(email) {
+    this.blacklistedEmails.push(email);
   }
 
-  changeBrand(newBrand) {
-    this.#brand = newBrand;
+  isBlacklisted(email) {
+    
+    return this.blacklistedEmails.includes(email)
+    
   }
+
   // Change code above this line
 }
 
-console.log(new Car({ brand: "Audi", model: "Q3", price: 36000 }))
+const mango = new Admin({
+  email: "mango@mail.com",
+  accessLevel: Admin.AccessLevel.SUPERUSER,
+});
+
+console.log(mango.email); // "mango@mail.com"
+console.log(mango.accessLevel); // "superuser"
+
+mango.blacklist("poly@mail.com");
+console.log(mango.blacklistedEmails); // ["poly@mail.com"]
+console.log(mango.isBlacklisted("mango@mail.com")); // false
+console.log(mango.isBlacklisted("poly@mail.com")); // true
+
